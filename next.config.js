fedafbadef570b06cf172c639c79c1dde2774ebf
Özgür Akgün <ozgurakgun@gmail.com>
2025-02-03 15:01:13 +0000

    Update next.config.js - remove another unrecognised field

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  future: { webpack5: true },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
